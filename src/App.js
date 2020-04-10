12e26ce11780efa512d07b92735799d9b9a8c000
ProjectTL@Badrul <badrul.reactjs.developer@gmail.com>
2020-04-10 21:48:08 +0530

    add the function in App.js by TL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 export default class App extends React.Component
 {
+  constructor()
+  {
+    super()
+    this.state={
+        name:"Badrul Haque",
+        age:22,
+        isSingal:true,
+        obj:{'PID':'LG-2345'}
+    }
+  }
    render()
    {
      return(
-       <div> </div>
+       <div className='App'> 
+       <h1 style={{color:'red'}}> My name is {this.state.name}</h1>
+       <h1 style={{color:'green'}}> and age is {this.state.age} </h1>
+       <h1 style={{color:'gray'}}> am i Singal {JSON.stringify(this.state.isSingal)} </h1>
+       <h1 style={{color:'yellow'}}> Product is {JSON.stringify(this.state.obj)} </h1>
+       </div>
      )
    }
 }
